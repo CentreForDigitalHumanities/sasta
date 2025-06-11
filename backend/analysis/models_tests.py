6af69965b5e86f9fde70ccfba0867bf05320409e
Jelte van Boheemen <j.vanboheemen@uu.nl>
2025-06-11 14:59:44 +0200

    Add variant parameter to to_sastadev method and enhance tests for query variants

--- backend/analysis/models_tests.py
+++ backend/analysis/models_tests.py
@@ -1,5 +1,6 @@
 from sastadev.readmethod import read_method
 from analysis.models import AssessmentQuery
+import pytest
 
 
 def test_sastadev_queries(asta_method):
@@ -16,3 +17,23 @@ def test_sastadev_queries(asta_method):
         # Assert that the queries remain untouched after
         # two-way conversion
         assert sd_obj.__dict__ == q.__dict__
+
+
+@pytest.mark.parametrize('variant', ['astae', 'astafuture'])
+def test_sastadev_queries_variants(asta_method, variant):
+    result = read_method(asta_method.category.name.lower(),
+                         asta_method.content.path)
+
+    variant_queries = [q.id for q in result.queries.values()
+                       if variant in q.variants]
+    other_variant_queries = [q.id for q in result.queries.values()
+                             if q.variants != [] and variant not in q.variants]
+
+    result_variant = read_method(asta_method.category.name.lower(),
+                                 asta_method.content.path, variant=variant)
+
+    assert len(result.queries) != len(result_variant.queries)
+    for q in variant_queries:
+        assert q in result_variant.queries
+    for q in other_variant_queries:
+        assert q not in result_variant.queries
