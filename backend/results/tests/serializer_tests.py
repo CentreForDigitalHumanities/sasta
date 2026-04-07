7676aa2e53e20db02fb69fad53122028ffe4c596
Jelte van Boheemen <j.vanboheemen@uu.nl>
2026-04-07 15:59:24 +0200

    Serialize complex keys in postresults

--- backend/results/tests/serializer_tests.py
+++ backend/results/tests/serializer_tests.py
@@ -1,6 +1,13 @@
 from xml.etree import ElementTree as etree
+from collections import Counter
 
-from results.serializers import allresults_from_dict, allresults_to_dict
+from sastadev.allresults import AllResults
+from results.serializers import (
+    allresults_from_dict,
+    allresults_from_json,
+    allresults_to_dict,
+    allresults_to_json,
+)
 
 
 def test_serializer(example_allresults):
@@ -19,3 +26,25 @@ def test_serializer(example_allresults):
     # assert allresults_in.analysedtrees.keys() == output.analysedtrees.keys()
     # for k in allresults_in.analysedtrees.keys():
     #     assert etree.tostring(allresults_in.analysedtrees[k]) == etree.tostring(output.analysedtrees[k])
+
+
+def test_json_serializer_supports_tuple_keys_in_postresults():
+    allresults_in = AllResults(
+        uttcount=1,
+        filename='single_utt',
+        coreresults={},
+        exactresults={},
+        allmatches={},
+        analysedtrees=[],
+        annotationinput=True,
+        allutts={'1': ['foo']},
+        postresults={
+            'A046': Counter({('beet', '1'): 1, ('ongeluk', '1'): 1}),
+            'A165': 'output_file_name',
+        },
+    )
+
+    json_str = allresults_to_json(allresults_in)
+    output = allresults_from_json(json_str)
+
+    assert allresults_in.postresults == output.postresults
