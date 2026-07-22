bd20f486dff988d886f5bd89742467cd18304933
Jelte van Boheemen <j.vanboheemen@uu.nl>
2026-07-22 10:31:08 +0200

    Update authentication settings

--- backend/sasta/common_settings.py
+++ backend/sasta/common_settings.py
@@ -112,7 +112,8 @@ REST_FRAMEWORK = {
 SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_LOGIN_METHODS = {'email', 'username'}
+ACCOUNT_SIGNUP_FIELDS = {'email*', 'username*', 'password1*', 'password2*'}
 
 # Celery
 CELERY_RESULT_BACKEND = 'django-db'
